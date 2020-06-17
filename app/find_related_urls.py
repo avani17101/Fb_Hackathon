d3b485d81537d8f97dcca87db52e41057c14f9d6
avani17101 <staravani1999@gmail.com>
2020-06-17 13:27:59 +0530

    rectified search function

---
+++ app/find_related_urls.py
@@ -0,0 +1,19 @@
+def find_related_urls(title,website):
+    """
+    args: title of article
+    returns: links of  most related articles from trusted sources
+    """
+    try: 
+        from googlesearch import search 
+    except ImportError:  
+        print("No module named 'google' found") 
+    
+    print(title)
+    related_urls = []
+    # to search 
+    query1 = website + title
+    print("Sugesstion")
+    for q in search(query1, tld="com", num=10, stop=1, pause=2): 
+        print(q)
+        related_urls.append(q)
+    return related_urls
